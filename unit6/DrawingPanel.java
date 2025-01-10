a17e9f4c5abc24969f808c75d3fb43c26ae6798d
Shuntia <128505810+shuntia@users.noreply.github.com>
2025-01-09 21:42:45 -0700

    update

--- unit6/DrawingPanel.java
+++ unit6/DrawingPanel.java
@@ -1,8 +1,31 @@
 
 import java.awt.Color;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import javax.swing.JPanel;
 public class DrawingPanel extends JPanel{
     private volatile int counter = 0;
+    public volatile ArrayList<Rect> rects=new ArrayList<Rect>();
+    class Rect{
+        int x;
+        int y;
+        int width;
+        int height;
+        Color color;
+        public Rect(int x, int y, int width, int height, Color color){
+            this.x=x;
+            this.y=y;
+            this.width=width;
+            this.height=height;
+            this.color=color;
+        }
+        public void draw(java.awt.Graphics g){
+            g.setColor(color);
+            g.fillRect(x, y, width, height);
+            g.setColor(Color.BLACK);
+            g.drawRect(x, y, width, height);
+        }
+    }
     public DrawingPanel(){
         super();
     }
@@ -11,10 +34,17 @@ public class DrawingPanel extends JPanel{
         counter++;
         int x=(int)(Math.random()*300);
         int y=(int)(Math.random()*300);
-        g.setColor(Color.GREEN);
-        g.fillRect(x, y, 100, 100);
-        g.setColor(Color.BLACK);
-        g.drawRect(x, y, 100, 100);
+        int w=(int)(Math.random()*200)+20;
+        int h=(int)(Math.random()*200)+20;
+        Color c=randomColor();
+        Rect r=new Rect(x, y, w, h, c);
+        rects.add(r);
+        for(Rect rect:rects){
+            rect.draw(g);
+        }
         g.drawString("System called paintComponent "+counter+" times.", 100,100);
+    }
+    Color randomColor(){
+        return new Color((int)(Math.random()*256), (int)(Math.random()*256), (int)(Math.random()*256));
     }
 }
